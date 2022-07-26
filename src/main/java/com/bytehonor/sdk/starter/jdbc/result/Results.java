2e230875ae9262d459e61f84cb890ff62d29c713
lijianqiang <newphper@126.com>
2022-07-26 15:46:55 +0800

    0.3.16

---
+++ src/main/java/com/bytehonor/sdk/starter/jdbc/result/Results.java
@@ -0,0 +1,32 @@
+package com.bytehonor.sdk.starter.jdbc.result;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
+
+import com.bytehonor.sdk.starter.jdbc.util.SqlColumnUtils;
+
+public class Results {
+
+    public static String string(ResultSet rs, String column) throws SQLException {
+        return rs.getString(columnLabel(column));
+    }
+
+    public static long longer(ResultSet rs, String column) throws SQLException {
+        return rs.getLong(columnLabel(column));
+    }
+
+    public static int integer(ResultSet rs, String column) throws SQLException {
+        return rs.getInt(columnLabel(column));
+    }
+
+    public static boolean bool(ResultSet rs, String column) throws SQLException {
+        return rs.getBoolean(columnLabel(column));
+    }
+
+    private static String columnLabel(String column) {
+        Objects.requireNonNull(column, "column");
+
+        return SqlColumnUtils.camelToUnderline(column);
+    }
+}
