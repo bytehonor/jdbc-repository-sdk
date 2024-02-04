446067926ee489bd2c1282c1d9e39f79e2633c78
lijianqiang <newphper@126.com>
2024-02-04 18:16:53 +0800

    underline

--- src/main/java/com/bytehonor/sdk/starter/jdbc/result/Results.java
+++ src/main/java/com/bytehonor/sdk/starter/jdbc/result/Results.java
@@ -9,26 +9,26 @@ import com.bytehonor.sdk.starter.jdbc.util.SqlColumnUtils;
 public class Results {
 
     public static String string(ResultSet rs, String column) throws SQLException {
-        return rs.getString(columnLabel(column));
+        return rs.getString(underline(column));
     }
 
     public static long longer(ResultSet rs, String column) throws SQLException {
-        return rs.getLong(columnLabel(column));
+        return rs.getLong(underline(column));
     }
 
     public static int integer(ResultSet rs, String column) throws SQLException {
-        return rs.getInt(columnLabel(column));
+        return rs.getInt(underline(column));
     }
 
     public static boolean bool(ResultSet rs, String column) throws SQLException {
-        return rs.getBoolean(columnLabel(column));
+        return rs.getBoolean(underline(column));
     }
 
     public static double doubler(ResultSet rs, String column) throws SQLException {
-        return rs.getDouble(columnLabel(column));
+        return rs.getDouble(underline(column));
     }
 
-    private static String columnLabel(String column) {
+    private static String underline(String column) {
         Objects.requireNonNull(column, "column");
 
         return SqlColumnUtils.camelToUnderline(column);
