807a02b5934ea9410a3d64293dbb0d240933ad13
lijianqiang <newphper@126.com>
2024-05-14 15:31:22 +0800

    0.6.0

--- src/main/java/com/bytehonor/sdk/starter/jdbc/config/JdbcStarterAutoConfiguration.java
+++ src/main/java/com/bytehonor/sdk/starter/jdbc/config/JdbcStarterAutoConfiguration.java
@@ -5,9 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.jdbc.JdbcProperties;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -20,14 +20,14 @@ import com.bytehonor.sdk.starter.jdbc.dao.JdbcProxyDao;
  */
 @Configuration
 @ConditionalOnClass({ DataSourceProperties.class, JdbcTemplate.class, JdbcProperties.class })
-@AutoConfigureAfter(DataSourceAutoConfiguration.class)
+@AutoConfigureAfter(JdbcTemplateAutoConfiguration.class)
 public class JdbcStarterAutoConfiguration {
 
     private static final Logger LOG = LoggerFactory.getLogger(JdbcStarterAutoConfiguration.class);
 
     @Bean
     @ConditionalOnProperty(prefix = "spring.datasource", name = "driver-class-name", matchIfMissing = false)
-    public JdbcProxyDao jdbcProxyDao(JdbcTemplate jdbcTemplate) {
+    JdbcProxyDao jdbcProxyDao(JdbcTemplate jdbcTemplate) {
         LOG.info("[Bytehonor] JdbcProxyDao");
         return new JdbcProxyDao(jdbcTemplate);
     }
